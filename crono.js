657dd1b53eea36105c43dafdb4e62597069ca443
Oussama Benzzi <ousambenz007@gmail.com>
2025-04-20 17:46:02 +0100

    Descritpion: chhronometre web avec 3 fonction simple: START, STOP et RESET

---
+++ crono.js
@@ -0,0 +1,53 @@
+var sp,btn_start,btn_stop,t,ms,s,mn,h;
+
+window.onload = function(){
+    sp = document.getElementsByTagName('span');
+    btn_start = document.getElementById('start');
+    btn_stop = document.getElementById('stop');
+    t;
+    ms=0, s=0, mn=0, h=0 ;
+}
+
+ function update_crono(){
+    ms+=1;
+    if(ms==10){
+        ms=0;
+        s+=1;
+    }
+    if(s==60){
+        s=0;
+        mn+=1;
+    }if(mn==60){
+        mn=0;
+        h+=1;
+    }
+
+    sp[0].innerHTML = h + "h:";
+    sp[1].innerHTML = mn + "min:";
+    sp[2].innerHTML = s + "s:";
+    sp[3].innerHTML = ms + "mn:";
+ }
+
+
+ function start(){
+    t = setInterval(update_crono,100);
+    btn_start.disabled  = true;
+ }
+
+ function stop(){
+    clearInterval(t);
+    btn_start.disabled = false
+ }
+
+ function reset(){
+    clearInterval(t);
+    btn_start.disabled = false;
+
+     ms=0, s=0, mn=0, h=0;
+
+
+    sp[0].innerHTML = h + "h:";
+    sp[1].innerHTML = mn + "min:";
+    sp[2].innerHTML = s + "s:";
+    sp[3].innerHTML = ms + "ms";
+ }
